6406fd0454da31cafab84cb20f29c51d3b8c9531
MZhiyenbayev <mzhiyenbayev@one.kz>
2022-04-28 11:55:04 +0600

    changed column name to region and date format to yyyy-mm-dd

--- scripts/process.py
+++ scripts/process.py
@@ -7,10 +7,11 @@ r = requests.get('https://taldau.stat.gov.kz/ru/Api/GetIndexData/704447?period=7
 raw_dt = json.loads(r.text)
 
 with open('data/output.csv', 'w', newline='') as file:
-    fieldnames = ['city', 'date', 'value']
+    fieldnames = ['region', 'date', 'value']
     writer = csv.writer(file)
     writer.writerow(fieldnames)
     for i in raw_dt:
         for j in range(len(i["periods"])):
-            writer.writerow((i["termNames"][0],i["periods"][j]['date'],i["periods"][j]['value'].split(".")[0]))
+            dt = '{year}-{month}-{day}'.format(year = i["periods"][j]['date'].split(".")[2], month = i["periods"][j]['date'].split(".")[1], day = i["periods"][j]['date'].split(".")[0])
+            writer.writerow((i["termNames"][0],dt,i["periods"][j]['value'].split(".")[0]))
             
